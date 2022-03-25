0b76ec024ef5cd1d7cddbb5daf0c486af808fb70
swarajRaut01 <102353148+swarajRaut01@users.noreply.github.com>
2022-03-25 14:44:06 +0530

    first program in python
    number in python and type conversion

---
+++ first.py
@@ -0,0 +1,32 @@
+#write a python program to numeric identify data type and type coversion
+
+#variable declaration
+num_1=100
+
+#data type checking
+print(type(num_1))
+
+#convert data to binary
+binnary = bin(num_1)
+print(binnary)
+
+#convert data to octal
+octal = oct(num_1)
+print(octal)
+
+
+#convert data to hexadecimal form
+hexa = hex(num_1)
+print(hexa)
+
+#convert data to float
+floating = float(num_1)
+print(floating)
+
+#convert to complex
+comp = complex(num_1,4)
+print(comp)
+
+
+#convert to int
+print(int(0b1010))
