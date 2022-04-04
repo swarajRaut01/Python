4d5e85a539611f41cae159bdfb5a6ea55a9517ee
swarajRaut01 <102353148+swarajRaut01@users.noreply.github.com>
2022-04-04 14:23:19 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+# if eles python program
+
+age=int(input("enter your age"))
+
+if age>=18:
+    print("your get job")
+
+
+else:
+    print("sry you have wait")
---
+++ ifelse2.py
@@ -0,0 +1,10 @@
+#if else with string
+
+name = input("Enter your name")
+
+if name == "radhika":
+    print("Welcome")
+
+else:
+    print("Plz enter valid name")
+    
