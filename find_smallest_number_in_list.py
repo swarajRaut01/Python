5c891ec7be85db3cd90e7795fb6565a4a51ae369
Swaraj raut <102353148+swarajRaut01@users.noreply.github.com>
2022-04-20 12:09:09 +0530

    Add files via upload

---
+++ find_smallest_number_in_list.py
@@ -0,0 +1,9 @@
+# smallest number in list
+
+def smallest_num_in_list( list ):
+    min = list[ 0 ]
+    for a in list:
+        if a < min:
+            min = a
+    return min
+print(smallest_num_in_list([1, 2, -8, 0]))
