b69010e94eb85465d32ab8f06f4fffcc7a93bde2
Swaraj raut <102353148+swarajRaut01@users.noreply.github.com>
2022-04-20 11:56:50 +0530

    Add files via upload

---
+++ list_module_program.py
@@ -0,0 +1,10 @@
+list1=[1,2,3,4,5,6,7,8,9]
+temp_list=list1[:]
+list1.sort()
+if temp_list==list1:
+    print("given list is in ascending order")
+
+else:
+    print("given list is not ascending order")
+
+    
