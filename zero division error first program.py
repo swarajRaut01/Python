28c4a6d70c14244b65c9bd2c264b7488a82a75a5
Swaraj raut <102353148+swarajRaut01@users.noreply.github.com>
2022-05-31 10:36:23 +0530

    Add files via upload

---
+++ zero division error first program.py
@@ -0,0 +1,11 @@
+try:
+    Risky 
+except:
+    handling code / alternative code
+    print("hello")
+
+try:
+    print(10/0)
+except zeroDivisionError :
+        print("plese chack inpute")
+        print("hiii")
